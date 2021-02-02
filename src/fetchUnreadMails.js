943cb9ed6522dec50b3333b94f73105f030bf7ce
jopemachine <gravityc95@gmail.com>
2021-02-02 13:19:24 +0900

    Update

--- src/fetchUnreadMails.js
+++ src/fetchUnreadMails.js
@@ -1,8 +1,10 @@
 const alfy = require('alfy')
 const process = require('process')
 const imaps = require('imap-simple')
-const config = require('../config.json')
 const _ = require('lodash')
+const config = require('../config.json')
+const usageCache = require('../cache.json')
+const fs = require('fs')
 
 // Avoids DEPTH_ZERO_SELF_SIGNED_CERT error for self-signed certs
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
@@ -12,41 +14,59 @@ process.removeAllListeners('warning');
   let unreadMails = []
   let totalRecordCnts = 0
 
-  await Promise.all(
-    _.map(Object.keys(config.accounts), async (account) => {
-      const imapConn = await imaps.connect(config.accounts[account])
-      await imapConn.openBox('INBOX')
+  if (new Date().getTime() - usageCache.date < config.cacheDuration) {
+    unreadMails = usageCache.cache
+    totalRecordCnts = usageCache.cache.length
+  } else {
+    await Promise.all(
+      _.map(Object.keys(config.accounts), async (account) => {
+        if (!config.accounts[account].enabled) return
 
-      const searchCriteria = ['UNSEEN']
+        const imapConn = await imaps.connect(config.accounts[account])
+        await imapConn.openBox('INBOX', false)
 
-      const fetchOptions = {
-        // bodies: ["HEADER", "TEXT"],
-        bodies: ['HEADER'],
-        markSeen: false
-      }
-
-      const results = await imapConn.search(searchCriteria, fetchOptions)
-
-      const mails = results.map(function (res) {
-        return {
-          title: config.servicePrefix
-            ? `[${account}] ` + res.parts[0].body.subject[0]
-            : res.parts[0].body.subject[0],
-          date: res.parts[0].body.date,
-          from: res.parts[0].body.from
+        const searchCriteria = ['UNSEEN']
+        const fetchOptions = {
+          // bodies: ["HEADER", "TEXT"],
+          bodies: ['HEADER'],
+          markSeen: config.autoMarkSeen
         }
+
+        const results = await imapConn.search(searchCriteria, fetchOptions)
+        const mails = results.map(function (res) {
+          return {
+            title: config.servicePrefix
+              ? `[${account}] ` + res.parts[0].body.subject[0]
+              : res.parts[0].body.subject[0],
+            date: res.parts[0].body.date[0],
+            from: res.parts[0].body.from[0],
+            provider: account
+          }
+        })
+
+        totalRecordCnts += mails.length
+        unreadMails = [...unreadMails, ...mails]
+
+        fs.writeFileSync(
+          'cache.json',
+          '\ufeff' +
+            JSON.stringify(
+              {
+                date: new Date().getTime(),
+                cache: unreadMails
+              },
+              null,
+              2
+            ),
+          { encoding: 'utf8' }
+        )
+        // await imapConn.imap.closeBox(true);
+        // await imapConn.end();
       })
+    )
+  }
 
-      totalRecordCnts += mails.length
-
-      unreadMails = [...unreadMails, ...mails]
-
-      // await imapConn.imap.closeBox(true);
-      // await imapConn.end();
-    })
-  )
-
-  let result
+  let result = []
   if (totalRecordCnts === 0) {
     result.push({
       title: 'No unread emails',
@@ -67,13 +87,19 @@ process.removeAllListeners('warning');
         autocomplete: '',
         arg: '',
         quicklookurl: '',
+        icon: {
+          path: config.accounts[mail.provider].icon
+            ? config.accounts[mail.provider].icon
+            : './icons/default.png'
+        },
         text: {
-          copy: '',
-          largetype: ''
+          copy: mail.title,
+          largetype: mail.title
         }
       }
     })
 
+    result.reverse()
     result.splice(0, 0, {
       title: `${unreadMails.length} emails were found.`,
       subtitle: 'Press Enter on this item to mark all the below emails',
